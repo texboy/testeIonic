bc8ccaee9e5f8c2a62235677b94a7f54c2d18204
texboy <victorblopes52@gmail.com>
2018-11-26 16:48:26 -0200

    Primeiro commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+
+  @ViewChild('mySlider') mySlider: any;
+
+  slides =  [
+             {
+               "img": "assets/img/login.jpg?t=-950866085quality=60&h=800",
+               "titulo": "Simule o quanto irá economizar!",
+               "texto": "Teste",
+               },
+               {
+                "img": "assets/img/login.jpg?t=-950866085quality=60&h=800",
+                "titulo": "Teste",
+                "texto": "Teste",
+                },
+           ]
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  slideNext(){
+    this.mySlider.slideNext();
+  }
+
+  slidePrev(){
+    this.mySlider.slidePrev();
+  }
+
+}
